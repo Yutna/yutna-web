476ca6a5af629e4771a589bf940dbbd62fa3e68a
Yutthana Siphuengchai <yutthana.s@outlook.com>
2022-10-14 01:09:43 +0700

    chore: update home page to support responsive design

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,13 @@ const HomePage: NextPage = () => {
       <Head>
         <title>Yutna - Home</title>
       </Head>
-      <main className="flex justify-center items-center w-screen h-screen text-white bg-gray-900">
-        <h1 className="text-9xl">Coming soon...</h1>
+      <main className="flex flex-col justify-center items-center w-screen h-screen bg-zinc-900">
+        <h1 className="text-white text-4xl md:text-6xl xl:text-8xl">
+          Coming soon...
+        </h1>
+        <p className="mt-4 text-white text-base md:mt-6 md:text-2xl xl:mt-8 xl:text-4xl">
+          This site is under construction 👷🏼‍♂️
+        </p>
       </main>
     </div>
   );
