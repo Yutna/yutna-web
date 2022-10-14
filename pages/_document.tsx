40e340e8d3bd2b21f828d990c625149b3c2eea80
Yutthana Siphuengchai <yutthana.s@outlook.com>
2022-10-14 21:19:24 +0700

    chore: setup i18n

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document(): JSX.Element {
   return (
-    <Html lang="en">
+    <Html>
       <Head>
         <link
           rel="stylesheet"
