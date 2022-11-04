12d3b82e4836269d71d543956218685fc51c2a65
Yutthana Siphuengchai <yutthana.s@outlook.com>
2022-11-05 00:44:11 +0700

    chore: add custom 500 page

--- locales/en/index.ts
+++ locales/en/index.ts
@@ -1,11 +1,13 @@
 import common from "./common";
 import home from "./home";
 import notFound from "./not-found";
+import serverSideError from "./server-side-error";
 
 const en = {
   common,
   home,
   notFound,
+  serverSideError,
 };
 
 export default en;
--- locales/th/index.ts
+++ locales/th/index.ts
@@ -1,11 +1,13 @@
 import common from "./common";
 import home from "./home";
 import notFound from "./not-found";
+import serverSideError from "./server-side-error";
 
 const th = {
   common,
   home,
   notFound,
+  serverSideError,
 };
 
 export default th;
