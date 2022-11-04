6cc9efb73bc976f5b9b4a706663ad9659fa6399c
Yutthana Siphuengchai <yutthana.s@outlook.com>
2022-11-05 06:58:43 +0700

    chore: add variable for next fonts

--- config/fonts.ts
+++ config/fonts.ts
@@ -3,12 +3,16 @@ import { Kanit, Taviraj, Azeret_Mono } from "@next/font/google";
 export const kanit = Kanit({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   style: ["normal", "italic"],
+  variable: "--font-kanit",
 });
 
 export const taviraj = Taviraj({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   style: ["normal", "italic"],
+  variable: "--font-taviraj",
 });
 
-// NOTE: Azeret Mono is a variable font, no need to add an option.
-export const azeretMono = Azeret_Mono();
+// NOTE: Azeret Mono is a variable font, no need to add weight and style options.
+export const azeretMono = Azeret_Mono({
+  variable: "--font-azeret-mono",
+});
